55ff57bd8ca26698bcbf8cf92cd5e3de3e6f1331
Geinersb <geinersb20@gmail.com>
2024-09-14 21:47:41 -0600

    se muestra el consumo y las cantidades

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import MenuItem from "./components/MenuItem";
+import OrderContents from "./components/OrderContents";
 import { menuItems } from "./data/db";
+import useOrder from "./hooks/useOrder";
 
 function App() {
+
+const {order, addItem} = useOrder()
+
   return (
     <>
       <header className="bg-teal-400 py-5">
@@ -12,17 +17,27 @@ function App() {
 
       <main className=" max-w-7xl mx-auto py-20 grid md:grid-cols-2">
         <div className="p-5">
-          <h2 className="text-4xl font-black">Menu</h2>
+          <h2 className="text-4xl font-black">Menú</h2>
 
           <div className="space-y-3 mt-10">
             {menuItems.map((item) => (
-              <MenuItem key={item.id} item={item} />
+              <MenuItem
+               key={item.id}
+                item={item}
+                addItem={addItem}
+                />
             ))}
           </div>
         </div>
 
-        <div>
-          <h2>Consumo</h2>
+        <div className="border border-dashed border-slate-300 p-5 rounded-lg space-y-10">
+            < OrderContents 
+         
+            order = {order}
+            />
+
+
+
         </div>
       </main>
     </>
--- src/types/index.ts
+++ src/types/index.ts
@@ -3,3 +3,8 @@ export type MenuItem = {
     name: string,
     price: number 
 }
+
+//con esto se toma lo mismo que tiene Menu Item pero le agregamos otra variable cantidad
+export type OrderItem = MenuItem & {
+    quantity : number
+}
